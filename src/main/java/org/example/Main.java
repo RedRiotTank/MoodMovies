b34f350284f41d815f6f79f335d0fcc51992f76c
Alberpm <albertoplazamontesdm@gmail.com>
2023-03-05 01:39:03 +0100

    Frist Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * @brief test brief
+ * @note test note.
+ * @author test auth.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
