556e4d9aefe7901ac8e1373f13f5d571b8fad304
Alberpm <albertoplazamontesdm@gmail.com>
2023-04-08 13:30:53 +0200

    add: Movie Structure

---
+++ src/main/java/backend/Movie.java
@@ -0,0 +1,67 @@
+package backend;
+
+import java.util.ArrayList;
+
+public class Movie {
+
+    private String name;
+    private int score;
+    private int year;
+    private String director;
+    private int id;
+
+    private ArrayList<Genre> genres;
+
+
+    public Movie(String name, int Score, int year, String director){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public ArrayList<Genre> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(ArrayList<Genre> genres) {
+        this.genres = genres;
+    }
+}
