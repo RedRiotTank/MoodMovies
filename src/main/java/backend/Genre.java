a651934273ff14bdb454f30bb4172f491f71f9f6
Alberpm <albertoplazamontesdm@gmail.com>
2023-04-08 13:32:42 +0200

    add: Genre structure

---
+++ src/main/java/backend/Genre.java
@@ -0,0 +1,22 @@
+package backend;
+
+public class Genre {
+    private int id;
+    private Tag tag;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Tag getTag() {
+        return tag;
+    }
+
+    public void setTag(Tag tag) {
+        this.tag = tag;
+    }
+}
