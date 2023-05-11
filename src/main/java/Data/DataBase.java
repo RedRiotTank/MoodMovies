e8371b30a877e66d04e7a4c96f54c67073a1dbcc
sergiozap <sergiozap1@gmail.com>
2023-05-11 20:05:19 +0200

    Métodos para sacar el nombre y el id de un genero, directamente desde la BD

--- src/main/java/Data/DataBase.java
+++ src/main/java/Data/DataBase.java
@@ -1,7 +1,9 @@
 package Data;
 
 import backend.Movie;
+import backend.Tag;
 
+import javax.xml.transform.Result;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -33,6 +35,31 @@ public class DataBase extends Connector {
             e.printStackTrace();
         }
     return null;
+    }
+
+    // Método que saca el nombre de un género a partir de un id
+    public String getGenreNameById(int id) throws SQLException {
+        PreparedStatement pstmt = conn.prepareStatement("SELECT Genres.name FROM Genres WHERE id=?");
+        pstmt.setInt(1, id);
+        ResultSet rs = pstmt.executeQuery();
+
+        while(rs.next()){
+            return rs.getString(1);
+        }
+
+        return null;
+    }
+
+    public int getGenreIdByName(Tag name) throws SQLException {
+        PreparedStatement pstmt = conn.prepareStatement("SELECT Genres.id FROM Genres WHERE name=?");
+        pstmt.setString(1, name.toString());
+        ResultSet rs = pstmt.executeQuery();
+
+        while(rs.next()){
+            return rs.getInt(1);
+        }
+
+        return 0;
     }
 
     public List<Movie> getMovieList(){
